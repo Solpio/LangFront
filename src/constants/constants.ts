df7e8884ae9b8dd1fa2d712900d40d3c2462c64c
Solpio <ssusolpio@gmail.com>
2022-10-05 21:28:02 +0400

    fix: api endpoint

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,3 @@
-export const API_ENDPOINT = process.env.REACT_APP_API_URL
-  ? process.env.REACT_APP_API_URL
-  : 'http://localhost:4444/'
+export const API_ENDPOINT =
+  process.env.REACT_APP_API_URL || 'http://localhost:4444/'
 export const MAX_WORDS_PER_PAGE = 20
